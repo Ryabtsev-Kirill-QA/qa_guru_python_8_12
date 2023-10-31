6f6be0a0b5507f500c585cff7256143487b5ed55
Kirill <ryabtsev231297@gmail.com>
2023-10-31 09:25:23 +0300

    little fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,6 +26,7 @@ def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 2.0
 
     yield browser
 
