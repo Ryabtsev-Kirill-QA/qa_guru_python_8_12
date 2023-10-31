fe81aa29b5e7fb39c615d094ce51f98056d2c692
Kirill <ryabtsev231297@gmail.com>
2023-10-31 09:06:09 +0300

    little fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
