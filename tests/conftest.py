bd77d2c701d9b2dc8d348886168f6ca6c4f55288
Kirill <ryabtsev231297@gmail.com>
2023-11-10 09:51:55 +0300

    add browser params and env file

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,9 +44,9 @@ def setup_browser(request):
         options=options
     )
 
-    browser.config.base_url = "https://demoqa.com"
     browser.config.driver = driver
     browser.config.timeout = 2.0
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
